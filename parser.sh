d2801fc15b4e9fbf322fe579e45de1acf5cf276b
Alexey Storozhev <dark.storoj@gmail.com>
2013-04-02 23:30:03 +0400

    added interactivity to parser.sh

--- parser.sh
+++ parser.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-awk -f parser.awk usbmuxd.log.3 > log.html
+if [ -z "$2" ]; then
+    echo "Raw packets data to html converter"
+    echo "Usage: parser.sh log_file output_file"
+    exit
+fi
+
+awk -f parser.awk $1 > $2
